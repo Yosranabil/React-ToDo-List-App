78b7cc8381ca9ea824a70015c636716af6a4923f
Yosra Nabil Shafik <98766158+Yosranabil@users.noreply.github.com>
2025-12-03 15:11:41 +0200

    Uploaded Project Files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ToDoList from './ToDoList.jsx'
+
+function App() {
+    return (
+        <div>
+            <ToDoList />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // note the '/client'
+import App from './App.jsx';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
